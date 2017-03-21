b599e40da0869b857945b4d81dfae14c14a069a1
Gaal Alexandru <gsx_16@yahoo.com>
2017-03-21 23:28:03 +0200

    Started ReceiveSMS function

--- SecSys_Software/SecSys_Software.ino
+++ SecSys_Software/SecSys_Software.ino
@@ -11,7 +11,7 @@ Version 1.0 - 2017.03.08
 #define ORANGE_CLAUDIU_1 0751538300
 #define ORANGE_CLAUDIU_2 0749256822
 
-#define SERIAL_DEBUG (0)
+#define SERIAL_DEBUG (1)
 
 #if SERIAL_DEBUG  //If Serial Debug is On, security functions are not used
   #undef SECURITY_FUNCTIONS
@@ -29,9 +29,10 @@ Version 1.0 - 2017.03.08
 #define WIRE_C_INPUT_2 (9)
 #define GSM_RX_Pin     (10)  //Pin to receive data from GSM module
 #define GSM_TX_Pin     (11)  //Pin to transmit data to GSM module
+#define GSM_Power_Pin  (12)  //Pin used to power on GSM module via software
 
 #define STARTUP_DELAY  (60)  //number of seconds to wait befor startup
-#define SMS_SEND_DELAY (4000)  //number of mili seconds to wait after sms sending
+#define SMS_SEND_DELAY (5000)  //number of mili seconds to wait after sms sending
 #define PIR_READ_DELAY (1000)  //number of mili seconds to wait between PIR reads
 
 #define ULTRA_SHORT_DELAY (10)  //10ms
@@ -41,17 +42,32 @@ Version 1.0 - 2017.03.08
 
 SoftwareSerial mySerial(GSM_RX_Pin, GSM_TX_Pin);  //10-RX, 11-TX
 /*--------------------Global variables declaration secion---------------------*/
+enum enum_sms_messages {
+  No_Message,
+  PIR_A,
+  PIR_B,
+  Wire_1_Pull,
+  Wire_1_Cut,
+  Wire_2_Pull,
+  Wire_2_Cut,
+  Wire_3_Pull,
+  Wire_3_Cut,
+  Status,
+  Wrong_Command
+} ;
+typedef enum_sms_messages sms_messages;
 unsigned char Active_System = 1;  //Global variable to activate / deactivate security system
 unsigned char Active_Alarm = 1;  //Global variable to activate / deactivate alarm
 unsigned char Trigger_Alarm = 0;  //Global variable to trigger alarm
 
 /*-------------------------------Setup secion--------------------------------*/
 void setup() {
-  unsigned char startup_delay = STARTUP_DELAY;
+  //unsigned long currentMillis = 0;
   
 #ifdef SECURITY_FUNCTIONS  //If Serial Debug is OFF
   pinMode(PIR_A_INPUT, INPUT);  //declare as input
   pinMode(PIR_B_INPUT, INPUT);
+  pinMode(GSM_Power_Pin, OUTPUT);
   
 /*
   pinMode(WIRE_A_INPUT_1, INPUT_PULLOP);  //Internal pullup resistor selected, use negative logic
@@ -63,15 +79,36 @@ void setup() {
 */
 #endif
 
+  PowerOnGSM();
   Serial.begin(9600);  //Setting the baud rate of serial communication with serial monitor
   mySerial.begin(9600);  //Setting the baud rate of GSM Module
   mySerial.println("AT+CMGF=1\r");  //convert the message style to text
   
+  mySerial.println("AT+CSQ");  //Signal Quality Report
+  //delay(2000);
+  //ShowSerialData();
+  mySerial.println("AT+CREG?");  //Attach or Detach  from Gprs Service
+  //delay(2000);
+  //ShowSerialData();
+  mySerial.println("AT+CMGF=1");  //convert the message style to text
+  //delay(2000);
+  //ShowSerialData();
+  mySerial.println("AT+CNMI=1,2,0,0,0");  //set SMS receive indications
+  //delay(2000);
+  //ShowSerialData();
+  
+//  while(startup_delay){
+//    delay(2*ULTRA_LONG_DELAY);  //wait 1 second
+//    startup_delay--; //decrement startup delay by 1 second
+//  }
+
   //wait for PIR sensors to stabilize
-  while(startup_delay){  //to wait 1 minute, see PIR datasheet
-    delay(2*ULTRA_LONG_DELAY);  //wait 1 second
-    startup_delay--; //decrement startup delay by 1 second
+#ifdef SECURITY_FUNCTIONS
+  //currentMillis = millis();
+  while(millis() < STARTUP_DELAY*1000){
+    //currentMillis = millis();    
   }
+#endif
   Serial.println("Setup ready!");
 }
 
@@ -114,11 +151,13 @@ void Process_PIR(){  //function to read and process PIR state
       //Trigger conditions met on PIR A
       PIR_A_State = HIGH;  //set current state high
       Serial.println("PIR A Triggered");
-      SendSMS(1);
+      SendSMS(PIR_A);
+      delay(ULTRA_SHORT_DELAY);
     }
     else if (PIR_A_State == HIGH) {
       PIR_A_State = LOW;
       Serial.println("PIR A Re-Armed");
+      delay(ULTRA_SHORT_DELAY);
     }
   }
 
@@ -126,14 +165,16 @@ void Process_PIR(){  //function to read and process PIR state
     previousMillisB = currentMillis;  // Remember the time    
     //Process PIR B data
     if ((PIR_B_Value == HIGH)&&(PIR_B_State == LOW)){  //Input was High and current state LOW
-      //Trigger conditions met on PIR A
+      //Trigger conditions met on PIR B
       PIR_B_State = HIGH;  //set current state high
       Serial.println("PIR B Triggered");
-      SendSMS(2);
+      SendSMS(PIR_B);
+      delay(ULTRA_SHORT_DELAY);
     }
     else if (PIR_B_State == HIGH) {
       PIR_B_State = LOW;
       Serial.println("PIR B Re-Armed");
+      delay(ULTRA_SHORT_DELAY);
     }
   }
 }
@@ -152,10 +193,23 @@ void Process_Wire_Guard(){
   Wire_B2 = !digitalRead(WIRE_B_INPUT_2);
   Wire_C1 = !digitalRead(WIRE_C_INPUT_1);
   Wire_C2 = !digitalRead(WIRE_C_INPUT_2);
-  //TODO
+
+  if(Wire_A1) {SendSMS(Wire_1_Pull);}
+  if(Wire_A2) {SendSMS(Wire_1_Cut);}
+  if(Wire_B1) {SendSMS(Wire_2_Pull);}
+  if(Wire_B2) {SendSMS(Wire_2_Cut);}
+  if(Wire_C1) {SendSMS(Wire_3_Pull);}
+  if(Wire_C2) {SendSMS(Wire_3_Cut);}
 }
-  
-void SendSMS(unsigned char send_message){
+
+void PowerOnGSM(void){
+  digitalWrite(GSM_Power_Pin, HIGH);
+  delay(1000);
+  digitalWrite(GSM_Power_Pin, LOW);
+  delay(7000);
+}
+
+void SendSMS(enum_sms_messages message){
   unsigned long currentMillis = millis();
   static unsigned long previousMillis = 0;
   static unsigned char smsState = LOW;
@@ -164,14 +218,32 @@ void SendSMS(unsigned char send_message){
     previousMillis = currentMillis;
     mySerial.println("AT+CMGS=\"0751538300\"\r");  //set the mobile number to send the SMS
     delay(ULTRA_LONG_DELAY);
-    switch (send_message) {
-      case 1:
+    switch (message) {
+      case PIR_A:
         mySerial.println("PIR A Triggered");  //The SMS text you want to send
         break;
-      case 2:
+      case PIR_B:
         mySerial.println("PIR B Triggered");
         break;
-      case 80: 
+      case Wire_1_Pull:
+        mySerial.println("Wire 1 Pulled");
+        break;
+      case Wire_1_Cut:
+        mySerial.println("Wire 1 Cut");
+        break;
+      case Wire_2_Pull:
+        mySerial.println("Wire 2 Pulled");
+        break;
+      case Wire_2_Cut:
+        mySerial.println("Wire 2 Cut");
+        break;
+      case Wire_3_Pull:
+        mySerial.println("Wire 3 Pulled");
+        break;
+      case Wire_3_Cut:
+        mySerial.println("Wire 3 Cut");
+        break;
+      case Status: 
         //Send security system status
         mySerial.println("Security system status:");
         //TODO
@@ -180,7 +252,7 @@ void SendSMS(unsigned char send_message){
         //Wire Guard State
         //PIR State
         break;
-      case 90:
+      case Wrong_Command:
         //Wrong SMS command received
         mySerial.println("Wrong Command. (1)Deactivate Security. (2)Deactivate Alarm. (3)Trigger Alarm. (4)Get Status");
         break;
@@ -216,11 +288,11 @@ unsigned char ReceiveSMS(){
       break;
     case 4:
       //Get Security Status
-      SendSMS(80);
+      SendSMS(Status);
       break;
     default:
       //Wrong message
-      SendSMS(90);
+      SendSMS(Wrong_Command);
   }
 }
 
@@ -245,11 +317,14 @@ void Serial_Debug(){
     }
     mySerial.println();
   }
+  Toggle_LED(150,150);
 #endif
 }
+
 /*--------------------------------Loop secion---------------------------------*/
 void loop() {
   Serial_Debug();
+
 #ifdef SECURITY_FUNCTIONS  //If Serial Debug is OFF
   if(Active_System){
     Toggle_LED(950,50);
