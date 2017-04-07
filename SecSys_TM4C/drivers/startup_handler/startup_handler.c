05dfddc07c6fed29d1502d45693adf2a78b1da02
Mihai Suteu <Mihai.Suteu-EXT@continental-corporation.com>
2017-04-08 02:35:50 +0300

    - added message after UART0 init to say system is starting up; - added message to indicate calibration of load cell is in progress;

--- SecSys_TM4C/drivers/startup_handler/startup_handler.c
+++ SecSys_TM4C/drivers/startup_handler/startup_handler.c
@@ -6,7 +6,6 @@
 /*-------------------Type Includes-------------------*/
 #include "stdbool.h"
 #include "stdint.h"
-
 /*-------------------HW define Includes--------------*/
 
 /*-------------------Driver Includes-----------------*/
@@ -16,17 +15,20 @@
 /*-----------------Application Includes---------------*/
 #include "GSM.h"
 #include "PIR.h"
+#include "HX711.h"
 /*-------------------Service Includes-----------------*/
 #include "gpio_handler.h"
 #include "uart_handler.h"
 /*-------------Global Variable Definitions------------*/
-
+extern uint32_t HX711_CalibVal;
 /*-------------Local Variable Definitions-------------*/
 
 /*-------------------Function Definitions-------------*/
 void InitDrivers(void) {
 	//Function calls to init drivers
 	UART0_Init();
+	UART0_SendString("System startup in progress...");
+	UART0_SendNewLine();
 	
 #if GSM_AVAILABLE
 	UART2_Init();
@@ -36,6 +38,15 @@ void InitDrivers(void) {
 	GPIO_InitPortInput(PortE, GPIO_PIN_2, GPIO_PIN_TYPE_STD_WPU);	// input init for port PE2
 	GPIO_InitPortOutput(PortE, GPIO_PIN_3);	// output init for port PE3 - SLK_HX711
 	GPIO_InitPortOutput(PortF, GPIO_PIN_2);
+	
+	UART0_SendString("Calibrating HX711...");
+	UART0_SendNewLine();
+	
+	HX711_CalibVal = HX711_Calibrate();
+	
+	UART0_SendString("HX711 Calibration value is : ");
+	UART0_SendUDecimal(HX711_CalibVal);
+	UART0_SendNewLine();
 #endif
 	
 	UART0_SendString("Driver init done...");
